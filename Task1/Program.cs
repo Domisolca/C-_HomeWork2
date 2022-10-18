9db618ffad838b37b2a5e7501d16b338e538f369
Olga <olya_potapova@mail.ru>
2022-10-18 10:49:22 +0300

    Add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = ReadInt("Введите трехзначное число: ");
+int min = 100, max = 999;
+
+if(number >= min && number <= max)
+{
+    int secondDigit = number / 10 % 10;
+    Console.WriteLine(secondDigit);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное!");
+}
