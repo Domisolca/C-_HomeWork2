720a17b81987047897f028424c3a569a7ddc75a1
Olga <olya_potapova@mail.ru>
2022-10-18 11:03:19 +0300

    Add Task 1_1 and Task 2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,10 +4,16 @@
     return Convert.ToInt32(Console.ReadLine());
 }
 
+bool IsBetween(int number, int min, int max)
+{
+    return number >= min && number <= max;
+}
+
+
 int number = ReadInt("Введите трехзначное число: ");
 int min = 100, max = 999;
 
-if(number >= min && number <= max)
+if(IsBetween(number, min, max))
 {
     int secondDigit = number / 10 % 10;
     Console.WriteLine(secondDigit);
---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool IsBetween(int number, int min, int max)
+{
+    return number >= min && number <= max;
+}
+
+int number = ReadInt("Введите число: ");
+int min = 100, max = 999;
+
+if(number >= min)
+{
+    int currentNumber = number;
+
+    while(IsBetween(currentNumber, min, max) == false)
+    {
+        currentNumber = currentNumber / 10;
+    }
+
+    int lastDigit = currentNumber % 10;
+    Console.WriteLine(lastDigit);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
