a48c6345cd2ea490559500afc45fcea73a93ab45
Olga <olya_potapova@mail.ru>
2022-10-18 11:14:09 +0300

    Add Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool IsBetween(int number, int min, int max)
+{
+    return number >= min && number <= max;
+}
+
+void PrintWeekend(int day)
+{
+    int sut = 6, sun = 7;
+
+    if(day == sut || day == sun)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+
+int day = ReadInt("Введите цифру: ");
+int min = 1, max = 7;
+
+if(IsBetween(day, min, max))
+{
+     PrintWeekend(day);
+}
+else
+{
+    Console.WriteLine("Такого дня не существует!");
+}
